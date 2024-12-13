35e56b9e813cab6a8340b7b8e4d624dde1429fbb
HarmonySpace <jchavaspace@gmail.com>
2024-12-12 23:07:02 -0600

    feat(core): add colors to tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      calPolyPormonaGreen: "#264531",
+      gunMetal: "#263045",
+      japaneseIndigo: "#264445",
+      japaneseIndigo2: "#263A45",
+      japaneseIndigo3: "#26453c",
+      carbonBlack: "#0c0a00",
+      whiteSmoke: "#F5F5F5",
+      ivory: "#fffff0",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+  },
+};
