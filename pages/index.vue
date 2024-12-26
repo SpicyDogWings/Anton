9ec45c314ff6d4a89bc9e10f340c0686b3e550b9
HarmonySpace <jchavaspace@gmail.com>
2024-12-25 22:41:17 -0600

    refactor(lucideIcons): remove lucideIcons to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 import { Query } from "appwrite";
-//import * as LucideIcons from "lucide-vue-next";
 
 onMounted(async () => {
   await getRestaurants();
 });
 
 const restaurants = ref();
-//const restaurant = ref({
-//  name: "",
-//  direction: "",
-//});
 const getRestaurants = async () => {
   try {
     const { listDocuments, documents, error } = useAppwriteDocuments();
@@ -27,27 +22,6 @@ const getRestaurants = async () => {
     console.log(e);
     restaurants.value = "no hay registros";
   }
-};
-
-//const addRestaurant = async () => {
-//  await appwrite.db.createDocument(
-//    connection.value.db,
-//    connection.value.restaurants,
-//    ID.unique(),
-//    {
-//      name: restaurant.value.name,
-//      direction: restaurant.value.direction,
-//      score: 1,
-//    },
-//  );
-//  await getRestaurants();
-//};
-
-//const getIcon = (iconName: string) => {
-//  return LucideIcons[iconName] || null;
-//};
-const getUrl = (id: string) => {
-  return `/restaurants/${id}`;
 };
 </script>
 <template>
@@ -74,43 +48,7 @@ const getUrl = (id: string) => {
             ></v-btn>
           </template>
         </v-card>
-        <!-- <a
-          class="w-fit h-fit flex flex-wrap justify-center items-center"
-          :href="getUrl(restaurant.$id)"
-        >
-          <div
-            class="py-8 px-8 flex justify-center items-center flex-grow border-r-[0.3rem]"
-            :style="{ 'background-color': '#' + restaurant.color }"
-          >
-            <component
-              :is="getIcon(restaurant.icon)"
-              class="text-white w-14 h-14"
-            />
-          </div>
-          <div
-            class="px-10 flex justify-center items-center flex-grow-[3] text-2xl"
-          >
-            <p class="text-gunMetal font-bold">
-              {{ restaurant.name }}
-            </p>
-          </div>
-        </a> -->
       </li>
     </ul>
   </section>
-  <!-- <form @submit.prevent="addRestaurant">
-    <v-btn variant="outlined" class="my-4" type="submit">
-      Añadir restaurante
-    </v-btn>
-    <v-text-field
-      label="Nombre del restaurante"
-      variant="outlined"
-      v-model="restaurant.name"
-    ></v-text-field>
-    <v-text-field
-      label="Dirección"
-      variant="outlined"
-      v-model="restaurant.direction"
-    ></v-text-field>
-  </form> -->
 </template>
