2e24ca82f92a744d2427e202d04a7d2edf68936b
HarmonySpace <jchavaspace@gmail.com>
2024-12-27 23:41:35 -0600

    feat(view): show platillos and add newPlatillos buton

--- pages/restaurants/[id].vue
+++ pages/restaurants/[id].vue
@@ -1,17 +1,50 @@
 <script setup lang="ts">
-const { getDocument, documents } = useAppwriteDocuments();
-
+import { Query } from "appwrite";
 const route = useRoute();
-const document = ref();
+const restaurant = ref();
+const plates = ref();
 
 onMounted(async () => {
+  const { getDocument, listDocuments, documents } = useAppwriteDocuments();
   await getDocument("main", "restaurants", String(route.params.id));
-  document.value = documents.value;
+  restaurant.value = documents.value;
+  await listDocuments("main", "plates", [
+    Query.equal("restaurant", String(route.params.id)),
+  ]);
+  plates.value = documents.value;
 });
 </script>
 <template>
   <a class="mt-10 flex justify-start items-center gap-4" href="/"
     ><LucideArrowLeft class="w-6 h-6 stroke-[0.2rem]" /> Volver</a
   >
-  <h1 class="mt-20 text-9xl font-general">{{ document?.name }}</h1>
+  <section class="w-full flex justify-center items-start flex-col">
+    <h1 class="mt-20 text-9xl font-general">{{ restaurant?.name }}</h1>
+    <v-btn
+      @click="navigateTo('/restaurants/' + restaurant.$id + '/plates/new')"
+      text="Agregar platillo"
+      variant="outlined"
+      class="mt-20 text-4xl"
+    >
+    </v-btn>
+    <h2 class="mt-20 text-4xl font-general">Platillos</h2>
+    <ul class="mt-5 w-full flex flex-wrap justify-start items-center gap-10">
+      <li v-for="plate in plates" :key="plate.$id">
+        <v-card>
+          <v-card-title>{{ plate.name }}</v-card-title>
+          <v-card-subtitle>{{ plate.description }}</v-card-subtitle>
+          <template v-slot:actions>
+            <v-btn
+              text="Ver"
+              @click="
+                navigateTo(
+                  '/restaurants/' + restaurant.$id + '/plates/' + plate.$id,
+                )
+              "
+            ></v-btn>
+          </template>
+        </v-card>
+      </li>
+    </ul>
+  </section>
 </template>
