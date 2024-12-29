76a01055c689a6acb7fe0e4ce0d19364753a9cf4
HarmonySpace <jchavaspace@gmail.com>
2024-12-29 11:37:26 -0600

    feat(view): update index view

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,14 +7,24 @@ useHead({
 </script>
 
 <template>
-  <section class="w-full h-screen flex justify-start items-center flex-col">
+  <section class="w-full h-full flex justify-start items-center flex-col bg-ivory">
     <section class="mt-20 w-10/12 flex justify-start items-start flex-col">
+      <menuBar class="mb-10" />
       <slot />
     </section>
   </section>
 </template>
 
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+* {
+  @apply font-general text-gunMetal shadow-gunMetal border-gunMetal decoration-gunMetal;
+}
+</style>
+
 <style>
 @import "assets/css/anton.css";
 @import "assets/css/general-sans.css";
-</style>
+</style
