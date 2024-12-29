e57a3f030d94e7463046cab8e3a18917ad7df916
HarmonySpace <jchavaspace@gmail.com>
2024-12-29 11:33:08 -0600

    feature(view): create a menuBar component

---
+++ components/menuBar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.path);
+const items = ref([
+  {
+    id: 1,
+    name: "Agregar restaurante",
+    link: "/restaurants/new",
+  },
+]);
+</script>
+<template>
+  <nav class="w-full">
+    <ul class="w-full flex justify-start items-center gap-10">
+      <li><a href="/" class="font-anton text-3xl">Anton</a></li>
+      <li v-for="item in items" :key="item.id">
+        <a
+          :href="item.link"
+          class="text-xl hover:cursor-pointer transition-all duration-200"
+          :class="{
+            'underline font-bold decoration-solid decoration-2':
+              route.path === item.link,
+          }"
+          >{{ item.name }}</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
