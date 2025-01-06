6414a7eb68d342750de30da1b40d207e96f02dd7
HarmonySpace <jchavaspace@gmail.com>
2025-01-06 00:47:24 -0600

    feat(view): refactor menu list layout

--- pages/index.vue
+++ pages/index.vue
@@ -15,12 +15,11 @@ const getRestaurants = async () => {
     const { listDocuments, documents, error } = useAppwriteDocuments();
     await listDocuments("main", "restaurants", [
       Query.orderDesc("$createdAt"),
-      Query.limit(5),
+      Query.limit(10),
     ]);
     if (error.value.bug) {
       throw new Error(error.value.message);
     }
-    console.log(documents.value);
     restaurants.value = documents.value;
   } catch (e) {
     console.log(e);
@@ -30,13 +29,6 @@ const getRestaurants = async () => {
 </script>
 <template>
   <div class="w-full flex flex-col justify-center items-start">
-    <!-- <section class="w-full flex justify-start items-end gap-10">
-      <h1 class="text-9xl font-anton">Carta</h1>
-      <span
-        class="py-2 px-6 text-xl font-general bg-ivory border-2 rounded-lg shadow-[10px_10px_0px_0px_#1a202c] -translate-y-4 hover:-translate-y-5 hover:-translate-x-1 transition-all duration-200 hover:shadow-[20px_20px_0px_0px] shadow-gunMetal"
-        >Todo tu menú en un solo lugar</span
-      >
-    </section> -->
     <h2 class="text-7xl font-general"></h2>
     <div class="w-full flex justify-center items-start flex-col">
       <section class="w-full flex flex-col justify-start items-start">
@@ -44,36 +36,39 @@ const getRestaurants = async () => {
           Carta de la casa
         </h1>
         <div class="flex justify-start items-start gap-20">
-          <p class="basis-80 text-lg">
-            Es momento de escojer uno de nuestros preferidos, ¿O será uno nuevo
-            esta vez?
+          <p class="basis-80 text-xl">
+            Es momento de escojer uno de nuestros preferidos
           </p>
           <p class="basis-80 text-lg">Todo tu menú en un solo lugar</p>
         </div>
       </section>
       <section class="w-full my-10">
         <div class="w-full flex justify-start items-center gap-5">
-          <LucideClock class="w-10 h-10 stroke-[0.15rem]" />
-          <h2 class="mb-2 text-5xl">Agregados recientemente</h2>
+          <h2 class="mb-2 font-bold text-5xl">Agregados recientemente</h2>
         </div>
         <v-divider :thickness="3" class="border-opacity-100"></v-divider>
-        <div>
-          <h3 class="text-3xl">Restaurantes</h3>
+        <div class="mt-5">
+          <h3 class="mb-2 font-bold text-3xl">Restaurantes</h3>
           <v-divider :thickness="1" class="border-opacity-100"></v-divider>
         </div>
       </section>
-      <ul class="mt-5 w-full flex flex-wrap justify-start items-center gap-10">
-        <li v-for="restaurant in restaurants" :key="restaurant.$id">
-          <v-card>
-            <v-card-title>{{ restaurant.name }}</v-card-title>
-            <v-card-subtitle>{{ restaurant.direction }}</v-card-subtitle>
-            <template v-slot:actions>
-              <v-btn
-                text="Ver"
-                @click="navigateTo('/restaurants/' + restaurant.$id)"
-              ></v-btn>
-            </template>
-          </v-card>
+      <ul
+        class="w-full columns-2 gap-10"
+      >
+        <li
+          class="w-full"
+          v-for="restaurant in restaurants"
+          :key="restaurant.$id"
+        >
+          <a
+            class="w-full flex justify-between items-center gap-5 hover:gap-10 hover:cursor-pointer  transition-all diration-200 ease-out"
+            @click="navigateTo('/restaurants/' + restaurant.$id)"
+          >
+            <span class="hover:pl-2 basis-fit text-nowrap font-bold text-xl transition-all duration-200 ease-out">{{
+              restaurant.name
+            }}</span>
+            <hr class="basis-full border-2 border-dotted"></hr>
+          </a>
         </li>
       </ul>
     </div>
