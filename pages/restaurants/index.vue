98598d578f76707d0d9e24ac0209ff9b3227f79c
HarmonySpace <jchavaspace@gmail.com>
2025-01-07 21:47:22 -0600

    feat(view): add restaurant main page

---
+++ pages/restaurants/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="w-full">
+    <ul class="w-full flex justify-start items-center gap-10">
+      <li><a href="/" class="font-anton text-3xl">Anton</a></li>
+    </ul>
+  </nav>
+</template>
