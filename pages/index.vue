8bc2c10850795da1adead8d9eb789478fddb9084
HarmonySpace <jchavaspace@gmail.com>
2025-01-07 21:45:11 -0600

    feat(view): update index view

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,11 @@ useHead({
 
 onMounted(async () => {
   await getRestaurants();
+  await getPlates();
 });
 
 const restaurants = ref();
+const plates = ref();
 const getRestaurants = async () => {
   try {
     const { listDocuments, documents, error } = useAppwriteDocuments();
@@ -24,6 +26,22 @@ const getRestaurants = async () => {
   } catch (e) {
     console.log(e);
     restaurants.value = "no hay registros";
+  }
+};
+const getPlates = async () => {
+  try {
+    const { listDocuments, documents, error } = useAppwriteDocuments();
+    await listDocuments("main", "plates", [
+      Query.orderDesc("$createdAt"),
+      Query.limit(10),
+    ]);
+    if (error.value.bug) {
+      throw new Error(error.value.message);
+    }
+    plates.value = documents.value;
+  } catch (e) {
+    console.log(e);
+    plates.value = "no hay registros";
   }
 };
 </script>
@@ -44,33 +62,50 @@ const getRestaurants = async () => {
       </section>
       <section class="w-full my-10">
         <div class="w-full flex justify-start items-center gap-5">
-          <h2 class="mb-2 font-bold text-5xl">Agregados recientemente</h2>
+          <h2 class="mb-2 text-5xl">Agregados recientemente</h2>
         </div>
         <v-divider :thickness="3" class="border-opacity-100"></v-divider>
-        <div class="mt-5">
-          <h3 class="mb-2 font-bold text-3xl">Restaurantes</h3>
+        <div class="my-5">
+          <h3 class="mb-2 text-3xl">Restaurantes</h3>
           <v-divider :thickness="1" class="border-opacity-100"></v-divider>
+          <ul class="my-5 w-full columns-2 gap-10">
+            <li
+              class="w-full"
+              v-for="restaurant in restaurants"
+              :key="restaurant.$id"
+            >
+              <a
+                class="w-full flex justify-between items-center gap-5 hover:gap-10 hover:cursor-pointer transition-all diration-200 ease-out"
+                @click="navigateTo('/restaurants/' + restaurant.$id)"
+              >
+                <span
+                  class="hover:pl-2 basis-fit text-nowrap font-bold text-xl transition-all duration-200 ease-out"
+                  >{{ restaurant.name }}</span
+                >
+                <hr class="basis-full border-2 border-dotted" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="my-5">
+          <h3 class="mb-2 text-3xl">Platos</h3>
+          <v-divider :thickness="1" class="border-opacity-100"></v-divider>
+          <ul class="my-5 w-full columns-2 gap-10">
+            <li class="w-full" v-for="plate in plates" :key="plate.$id">
+              <a
+                class="w-full flex justify-between items-center gap-5 hover:gap-10 hover:cursor-pointer transition-all diration-200 ease-out"
+                @click="navigateTo('/restaurants/' + plate.$id)"
+              >
+                <span
+                  class="hover:pl-2 basis-fit text-nowrap font-bold text-xl transition-all duration-200 ease-out"
+                  >{{ plate.name }}</span
+                >
+                <hr class="basis-full border-2 border-dotted" />
+              </a>
+            </li>
+          </ul>
         </div>
       </section>
-      <ul
-        class="w-full columns-2 gap-10"
-      >
-        <li
-          class="w-full"
-          v-for="restaurant in restaurants"
-          :key="restaurant.$id"
-        >
-          <a
-            class="w-full flex justify-between items-center gap-5 hover:gap-10 hover:cursor-pointer  transition-all diration-200 ease-out"
-            @click="navigateTo('/restaurants/' + restaurant.$id)"
-          >
-            <span class="hover:pl-2 basis-fit text-nowrap font-bold text-xl transition-all duration-200 ease-out">{{
-              restaurant.name
-            }}</span>
-            <hr class="basis-full border-2 border-dotted"></hr>
-          </a>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
